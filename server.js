f4d667a7eff950acde47c910732b90fe88734d47
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-11-20 11:51:40 +0300

    change server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const PORT = process.eng.PORT || 80;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -8,6 +9,6 @@ app.get('/*', function(req, res) {
 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000, () => {
-	console.log('Server running port : ', 9000);
+app.listen(PORT, () => {
+	console.log('Server running port : ', PORT);
 });
